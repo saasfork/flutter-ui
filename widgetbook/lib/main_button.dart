f3053d376d765213581a8f2e651b10c6567bc501
nicodevelop <nicolas@nico-develop.com>
2025-03-14 08:49:57 +0100

    Ajout de la possibilité de personnaliser le label du bouton dans l'exemple d'utilisation du MainButton

--- widgetbook/lib/main_button.dart
+++ widgetbook/lib/main_button.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:saasfork_design_system/saasfork_design_system.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'Default', type: MainButton)
 Widget buildCoolButtonUseCase(BuildContext context) {
-  return Center(child: MainButton(label: 'Cool Button', onPressed: () {}));
+  final label = context.knobs.string(
+    label: 'Button Label',
+    initialValue: 'Action',
+    description: 'Text displayed on the button',
+  );
+
+  return Center(child: MainButton(label: label, onPressed: () {}));
 }
